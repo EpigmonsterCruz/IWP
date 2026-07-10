f6d82867edce905e34e6dec615e2d8ce8f6720c5
EpigmonsterCruz <kiritoteutli@gmail.com>
2026-07-09 19:00:28 -0700

    Delete bookshelf directory

--- bookshelf/controller/contact.php
+++
@@ -1,4 +0,0 @@
-<?php
-$heading="Contact Us";
-
- view('contact.view.php', ['heading' => $heading ]);
--- bookshelf/views/partials/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html class="h-full bg-gray-100">
-  <head>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
-  </head>
-  <body>
-    <div class="min-h-full">
